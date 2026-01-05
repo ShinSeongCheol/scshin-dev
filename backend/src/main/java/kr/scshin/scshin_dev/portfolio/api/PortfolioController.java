2b444eb6bf9f99c7c723e8d352706d67093622a9
seongcheol <cher3725@gmail.com>
2026-01-06 02:13:32 +0900

    feat '스프링 타임리프'
    - 타임리프 설정 추가
    - portfolio 뷰 반환 컨트롤러 추가
    - portfolio 뷰 추가

---
+++ backend/src/main/java/kr/scshin/scshin_dev/portfolio/api/PortfolioController.java
@@ -0,0 +1,12 @@
+package kr.scshin.scshin_dev.portfolio.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PortfolioController {
+    @GetMapping("/portfolio")
+    public String portfolio() {
+        return "portfolio/index";
+    }
+}
