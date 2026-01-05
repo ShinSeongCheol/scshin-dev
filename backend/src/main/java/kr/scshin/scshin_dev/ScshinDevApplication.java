1c15d8efa17d77916b12c8a976c1682672e5a135
shinseongcheol <cher3725@naver.com>
2026-01-06 01:42:01 +0900

    init scshin-dev

---
+++ backend/src/main/java/kr/scshin/scshin_dev/ScshinDevApplication.java
@@ -0,0 +1,13 @@
+package kr.scshin.scshin_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScshinDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScshinDevApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kr/scshin/scshin_dev/ScshinDevApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.scshin.scshin_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScshinDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
