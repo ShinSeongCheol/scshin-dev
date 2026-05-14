29643ac51b74ba82ea5e64bed65efa1041bbecf7
shinseongcheol <cher3725@gmail.com>
2026-05-14 15:51:25 +0900

    :sparkles: feat '마크다운 파서'
    - 본문 글 요약 이미지 제거 추가

--- backend/src/main/java/kr/scshin/scshin_dev/blog/adapter/out/markdown/MarkdownParserAdapter.java
+++ backend/src/main/java/kr/scshin/scshin_dev/blog/adapter/out/markdown/MarkdownParserAdapter.java
@@ -2,10 +2,7 @@ package kr.scshin.scshin_dev.blog.adapter.out.markdown;
 
 import kr.scshin.scshin_dev.blog.application.port.out.MarkdownParsePort;
 import lombok.extern.slf4j.Slf4j;
-import org.commonmark.node.AbstractVisitor;
-import org.commonmark.node.BlockQuote;
-import org.commonmark.node.Image;
-import org.commonmark.node.Node;
+import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.text.TextContentRenderer;
 import org.springframework.stereotype.Component;
@@ -22,6 +19,14 @@ public class MarkdownParserAdapter implements MarkdownParsePort {
     @Override
     public String renderAsSummary(String markdown) {
         Node document = parser.parse(markdown);
+
+        document.accept(new AbstractVisitor() {
+            @Override
+            public void visit(Image image) {
+                image.unlink();
+            }
+        });
+
         return textRenderer.render(document);
     }
 
